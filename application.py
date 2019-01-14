1d4e7caae157dc4a37789995a2add509f80232f6
Iva Shenk <ivatje@live.nl>
2019-01-14 17:06:41 +0000

    toevoegen logout register, begin homepage

--- application.py
+++ application.py
@@ -28,6 +28,14 @@ Session(app)
 # configure CS50 Library to use SQLite database
 db = SQL("sqlite:///finance.db")
 
+@app.route("/homepage", methods=["GET", "POST"])
+def homepage():
+    """"homepage of site""""
+    if request.methode == "GET":
+        return render_template("homepage.html")
+
+
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -63,3 +71,62 @@ def login():
     # else if user reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out."""
+
+    # forget any user_id
+    session.clear()
+
+    # redirect user to login form
+    return redirect(url_for("homepage"))
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user."""
+    # forget any user_id
+    session.clear()
+
+    # if user reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username")
+
+        # query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = :username", username=request.form.get("username"))
+
+        # ensure username doesn't already exist
+        if len(rows) == 1:
+            return apology("username already exists")
+
+        # ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password")
+
+        # ensure password is the same as passwordcheck
+        elif request.form.get("password") != request.form.get("confirmation"):
+            return apology("passwords are not matching")
+
+        # encrypt password
+        myctx = CryptContext(schemes=["sha256_crypt"],
+                             sha256_crypt__default_rounds=80000)
+        hash = pwd_context.hash(request.form.get("password"))
+
+        # insert user into users
+        db.execute("INSERT INTO users (username, hash) VALUES(:username, :hash)", username=request.form.get("username"), hash=hash)
+        rows = db.execute("SELECT * FROM users WHERE username = :username", username=request.form.get("username"))
+
+        # remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # redirect user to home page
+        return redirect(url_for("index"))
+
+    # else if user reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("register.html")
